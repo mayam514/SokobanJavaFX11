fd3b119313c35967e3f62b05c67e7814aca3e201
Maya Mill <mayam514@gmail.com>
2017-06-11 17:20:27 +0300

    organize external jars

--- src/searchLib/Searchable.java
+++
@@ -1,9 +0,0 @@
-package searchLib;
-
-import java.util.HashMap;
-
-public interface Searchable<T> {
-	State<T> getInitialState();
-	State<T> getGoalState();
-	HashMap<Action, State<T>> getAllPossibleStates(State<T> state);
-}
